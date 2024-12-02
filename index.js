c0a33d2396cbc190d01b599254b45ecf701eb894
viz-nu <vishnu.teja101.vt@gmail.com>
2024-12-02 14:54:31 +0530

    fix v1

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-import  express from'express'
+import express from 'express'
+import { getContext, getResponse } from './utils/helper.js';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 const app = express()
 const port = 3000
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"))
+app.use(express.json({ type: ["application/json", "text/plain"], limit: '50mb' }));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+app.post('/v1/chat-bot', async (req, res) => {
+    const { userMessage } = req.body;
+    const contexts = await getContext(userMessage)
+    try {
+        const response = await getResponse(contexts, userMessage);
+        const botMessage = response.choices[0].message.content;
+        res.status(200).send({ success: true, data: botMessage })
+    } catch (error) {
+        console.error("Error with chatbot API:", error);
+    }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
---
+++ utils/helper.js
@@ -0,0 +1,64 @@
+import { MongoClient } from "mongodb";
+import OpenAI from "openai";
+import "dotenv/config"
+export const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
+export const EmbeddingFunct = async (text) => {
+    try {
+        const { data } = await openai.embeddings.create({
+            model: "text-embedding-3-small",
+            input: text,
+            encoding_format: "float",
+        });
+        return data[0].embedding;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+export const getContext = async (userMessage) => {
+    try {
+        const url = process.env.MONGO_URL;
+        const dbName = 'CBU';
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        let context = await db.collection('embeddings').aggregate([
+            {
+                $vectorSearch: {
+                    "queryVector": await EmbeddingFunct(userMessage),
+                    "path": "embedding",
+                    "numCandidates": 100,
+                    "limit": 10,
+                    "index": "cbu_vector_index"
+                }
+            },
+            { $project: { text: 1 } }
+        ]).toArray()
+        client.close();
+        return context;
+
+    } catch (error) {
+        client.close();
+        console.log(error);
+        return null;
+    }
+}
+export const getResponse = async (contexts, userMessage) => {
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{
+                "role": "system",
+                "content": "You are a helpful chatbot designed to assist users with information about Christian Brothers University (CBU). You only provide information related to the university and do not answer questions unrelated to CBU."
+            }, {
+                role: "user",
+                content: `Here is some information that exists in database which might help you relate to the user's query: 
+                    ${contexts.map(context => context.text).join('\n')}\n
+                    Use this information to answer the following questions concisely:  "${userMessage}"`
+            }],
+        });
+        return response
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
