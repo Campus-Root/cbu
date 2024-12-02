29e318b531bcea2c4b5cc0f1987cb9308f7629ed
viz-nu <vishnu.teja101.vt@gmail.com>
2024-12-03 01:18:22 +0530

    fix: path and dirname

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from "cors"
 import path from 'path';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const port = 3000
 app.use(cors());
